ca6338f410e6b3a4a898418e9a98b4dfb2b9e120
Lynn <lynn@foldr.moe>
2017-06-11 01:24:01 +0200

    Add files via upload

---
+++ jvs-json.rb
@@ -0,0 +1,16 @@
+#!/bin/ruby
+require 'json'
+require 'nokogiri'
+
+entires = []
+xml = File.open("en.xml") { |f| Nokogiri::XML(f) }
+xml.xpath("/dictionary[1]/direction[1]/valsi").each do |v|
+    entry = {word: v[:word], type: v[:type]}
+    entry[:gloss] = v.xpath("glossword").map(&:attributes)
+    entry[:def] = v.xpath("definition").text
+    selmaho = v.xpath("selmaho").text
+    entry[:selmaho] = selmaho if selmaho
+    entires << entry
+end
+
+puts JSON.generate(entires)
