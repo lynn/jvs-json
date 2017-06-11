9372b8f4d8ccdbba1ce2e1b3968b546283304cb7
Lynn <lynn@foldr.moe>
2017-06-11 14:35:19 +0200

    Add separate gismu list

--- jvs-json.rb
+++ jvs-json.rb
@@ -2,15 +2,23 @@
 require 'json'
 require 'nokogiri'
 
-entires = []
+entries = []
+gismus = []
 xml = File.open("en.xml") { |f| Nokogiri::XML(f) }
 xml.xpath("/dictionary[1]/direction[1]/valsi").each do |v|
-    entry = {word: v[:word], type: v[:type]}
-    entry[:gloss] = v.xpath("glossword").map(&:attributes)
-    entry[:def] = v.xpath("definition").text
-    selmaho = v.xpath("selmaho").text
-    entry[:selmaho] = selmaho unless selmaho.empty?
-    entires << entry
+  entry = {word: v[:word], type: v[:type]}
+  entry[:gloss] = v.xpath("glossword").map(&:attributes)
+  entry[:def] = v.xpath("definition").text
+  selmaho = v.xpath("selmaho").text
+  entry[:selmaho] = selmaho unless selmaho.empty?
+  entry[:rafsi] = v.xpath("rafsi").map(&:text)
+  entries << entry
+  if v[:type] == "gismu" then
+    entry = entry.dup
+    entry.delete(:type)
+    gismus << entry
+  end
 end
 
-puts JSON.generate(entires)
+File.open("en.json", "w") { |f| JSON.dump(entries, f) }
+File.open("en-gismu.json", "w") { |f| JSON.dump(gismus, f) }
